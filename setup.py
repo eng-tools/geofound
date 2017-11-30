81964fd6b05c83fcf5b99f34847a18ac0f7e7d47
millen1m <maxim.millen@gmail.com>
2017-11-30 13:30:51 +0000

    updated metadata preparing for pypi.

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from setuptools import setup, find_packages
 setup(name='geofound',
       version='0.2.0',
       description='A package to assess the bearing capacity and settlement of geofound',
-      url='',
+      url='https://github.com/eng-tools/geofound',
       author='Maxim Millen',
       author_email='mmi46@uclive.ac.nz',
-      keywords=['geotechnical', 'engineering', 'structural'],
+      keywords='geotechnical engineering foundations settlement',
       license='MIT',
       classifiers=[
           'Development Status :: 3 - Alpha',
-          'Intended Audience :: Scientific community',
-          'Topic :: Civil Engineering',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering'
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3',
       ],
       packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-      install_requires=[],
+      install_requires=['sfsimodels'],
       # List additional groups of dependencies here (e.g. development
       # dependencies). You can install these using the following syntax,
       # for example:

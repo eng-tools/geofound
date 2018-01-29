3399329a86df4349eeab02dbc4964624a211e02d
Fabrizio <fabrizio.panico@fe.up.pt>
2018-01-29 16:10:07 +0000

    Changed default gwl to 10000. corrected issue with schmertmann for long foundation and added a new test. added example to readme file.

--- tests/test_settlement.py
+++ tests/test_settlement.py
@@ -22,7 +22,27 @@ def test_schmertmann():
     sett = settlement.schmertmann_settlement(sl, fd, load, youngs_modulus_soil, unit_sat_weight=19.8, gwl=0.8)
 
     assert ct.isclose(sett, 0.017, rel_tol=0.01)
-    
+
+
+def test_schmertmann_long_foundation():
+    """
+    Checks settlement value against that from xxx
+    """
+    dry_unit_dry_weight = 18.0
+    length = 26
+    width = 2.5
+    depth = 0.8
+    phi = 0.0
+    cohesion = 20
+    unit_dry_weight = 17
+    sl = gm.create_soil(phi, cohesion, unit_dry_weight)
+    fd = gm.create_foundation(length, width, depth)
+    load = 1000 + (23.5 * 0.5 + dry_unit_dry_weight * 0.3) * length * width  # kN
+    youngs_modulus_soil = 3 * 6e3  # kPa
+    sett = settlement.schmertmann_settlement(sl, fd, load, youngs_modulus_soil, unit_sat_weight=19.8, gwl=0.8)
+    print(sett)
+    assert ct.isclose(sett, 0.00153, rel_tol=0.01)
+
 
 if __name__ == '__main__':
-    test_schmertmann()
+    test_schmertmann_long_foundation()
